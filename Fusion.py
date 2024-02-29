d8854ed217a328c783cf8ed79300148a04da7a47
LiYu <126240018+LiYu51@users.noreply.github.com>
2024-02-29 16:43:35 +0800

    Fusion.py

--- Fusion.py
+++ Fusion.py
@@ -7,7 +7,6 @@ import torch.nn.functional as F
 import numpy as np
 import math
 
-
 class ChannelPool(nn.Module):
     def forward(self, x):
         return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
