0b18e4755b4476bd5593670bceef571dfe1c23c6
LiYu <126240018+LiYu51@users.noreply.github.com>
2024-02-29 16:17:10 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -25,7 +25,27 @@ class BCEDiceLoss(nn.Module):
         dice = (2. * intersection.sum(1) + smooth) / (input.sum(1) + target.sum(1) + smooth)
         dice = 1 - dice.sum() / num
         return 0.5 * bce + dice
+        
+def binary_cross_entropy_loss(pred, target):
+    return F.binary_cross_entropy(pred, target)
 
+def ssim_loss(pred, target):
+    return 1 - ssim(pred, target, data_range=target.max() - target.min())
+
+def iou_loss(pred, target):
+    intersection = (pred & target).float().sum()
+    union = (pred | target).float().sum()
+    iou = intersection / union
+    return 1 - iou
+
+def mixed_loss(gc_branch_output, unet_output,f_output,gt):
+    bce_loss_gc = binary_cross_entropy_loss(gc_branch_output, gt)
+    ssim_loss_unet = ssim_loss(unet_output, gt)
+    iou_loss_f = iou_loss(f_output, gt)
+    
+    total_loss = bce_loss_gc + ssim_loss_unet + iou_loss_f
+    
+    return total_loss
 
 class LovaszHingeLoss(nn.Module):
     def __init__(self):
